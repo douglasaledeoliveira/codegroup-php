08536eed3d29639865b73c047d1fa59ceedce02a
Douglas A. de Oliveira <64898678+douglasaledeoliveira@users.noreply.github.com>
2024-01-22 13:11:08 -0300

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,13 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'https://codegroup-54552efc0bf4.herokuapp.com/players/create',
-        'https://codegroup-54552efc0bf4.herokuapp.com/players/edit/*',
-        'https://codegroup-54552efc0bf4.herokuapp.com/players/delete/*',
-        'https://codegroup-54552efc0bf4.herokuapp.com/players/store',
-        'https://codegroup-54552efc0bf4.herokuapp.com/players/update/*',
         'https://codegroup-54552efc0bf4.herokuapp.com/players',
-        'https://codegroup-54552efc0bf4.herokuapp.com/sort-teams',
-        'https://codegroup-54552efc0bf4.herokuapp.com'
+        'https://codegroup-54552efc0bf4.herokuapp.com/players/*',
     ];
 }
